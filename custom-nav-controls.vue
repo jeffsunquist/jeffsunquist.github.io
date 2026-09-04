e32d779d59ebe9bc140d4daa821fefdfdb9c65d9
jeffsunquist <jeffsunquist@outlook.com>
2026-09-04 20:08:32 +0800

    homepage link in nav bar

---
+++ custom-nav-controls.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const link = 'https://www.basado.org'
+</script>
+
+<template>
+  <a
+    class="slidev-icon-btn"
+    :href="link"
+    target="_blank"
+    rel="noopener"
+    title="Open basado.org"
+  >
+    <div class="i-carbon:link" />
+  </a>
+</template>
